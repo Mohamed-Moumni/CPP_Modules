12e9cd64866870afa25acc90ddf9025352469f13
Mohamed-Moumni <mohamedmoumni593@gmail.com>
2023-03-15 23:12:33 +0100

    handling arg error

---
+++ CPP_DAY09/ex00/main.cpp
@@ -0,0 +1,11 @@
+#include "BitcoinExchange.hpp"
+
+int main(int ac, char *av[])
+{
+    if (ac != 2)
+    {
+        std::cout << "Error: could not open file.\n";
+        exit(1);
+    }
+    return (0);
+}
